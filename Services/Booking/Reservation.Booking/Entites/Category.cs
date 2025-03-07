268e255d3533e15d7ecd08c138c6edb3dca57d30
MuratYucedag <yucedagmurat23@gmail.com>
2025-03-07 22:07:18 +0300

    Add project files.

---
+++ Services/Booking/Reservation.Booking/Entites/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Reservation.Booking.Entites
+{
+    public class Category
+    {
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
